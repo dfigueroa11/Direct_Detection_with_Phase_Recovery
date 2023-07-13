8018724de55e9f5795976b39377b8ec5e784089a
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-13 17:47:22 +0200

    first steps for implementing the system in numpy

--- code/DD_system.py
+++ code/DD_system.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 
 class DD_system():
 
@@ -6,6 +6,7 @@ class DD_system():
     N_os = None
     pulse_shape = None
     beta_2 = None
+    fiber_alpha = None 
     fiber_length = None
     Ts = None
     symbol_time = None
@@ -23,28 +24,24 @@ class DD_system():
 
 
     def pulse_shaping(self,symbols):
-        symbols_up_samp = torch.zeros(int(len(symbols)*self.N_sim), dtype=torch.cfloat)
+        symbols_up_samp = np.zeros(int(len(symbols)*self.N_sim), dtype=complex)
         symbols_up_samp[::self.N_sim] = symbols
-        return torch.functional.conv1d(symbols_up_samp,self.pulse_shape)
+        return np.convolve(symbols_up_samp,self.pulse_shape)
     
-    def applay_channel(self,symbols):
-        signal = torch.zeros(int(len(symbols)*self.N_sim), dtype=torch.cfloat)
-        signal[::self.N_sim] = symbols
-        n_fft = int(2**torch.ceil(torch.log2(torch.tensor([len(signal)]))))
-        pulse_shape_fft = torch.fft.fft(self.pulse_shape, n=n_fft)
-        f = torch.arange(0,n_fft)/n_fft*self.Bw
-        channel_response_fft = torch.exp(1j*self.beta_2*self.fiber_length*(2*torch.pi*f)**2/2)
-        print(len(channel_response_fft))
-        print(len(pulse_shape_fft))
-        signal_fft = torch.fft.fft(signal, n=n_fft)
-        out = torch.fft.ifft(channel_response_fft*signal_fft*pulse_shape_fft, n=n_fft)
+    def applay_channel(self,signal):
+        n_fft = int(16*2**np.ceil(np.log2(len(signal))))
+        f = self.Bw * np.fft.fftfreq(n_fft)
+        alpha_lin = self.fiber_alpha / (10 * np.log10(np.exp(1)))
+        channel_response_fft = np.exp(-alpha_lin*self.fiber_length+1j*self.beta_2*self.fiber_length*(2*np.pi*f)**2/2)
+        signal_fft = np.fft.fft(signal, n=n_fft)
+        out = np.fft.ifft(channel_response_fft*signal_fft, n=n_fft)
         return out[:len(signal)]
     
     def square_law_detection(self,signal):
-        abs_signal = torch.abs(signal)
+        abs_signal = np.abs(signal)
         square_law_signal = self.responsivity*abs_signal**2
-        shot_noise = abs_signal * torch.normal(0., self.sigma_sh, size=(1,len(signal)))
-        thermal_noise = torch.normal(0., self.sigma_th, size=(1,len(signal)))
+        shot_noise = abs_signal * np.random.normal(0, self.sigma_sh, size=(len(signal)))
+        thermal_noise = np.random.normal(0, self.sigma_th, size=(len(signal)))
         return square_law_signal + (shot_noise + thermal_noise)*self.on_off
     
     
--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 import DD_system
 import matplotlib.pyplot as plt
 
@@ -6,23 +6,27 @@ import matplotlib.pyplot as plt
 system = DD_system.DD_system()
 system.N_sim = 31
 system.beta_2 = -2.168e-23
+system.fiber_alpha = 0.2
 system.fiber_length = 30
-system.symbol_rate = 35e9
-system.sigma_sh = 0.
-system.sigma_th = 0.
+system.symbol_rate = 15e9
+system.sigma_sh = 0
+system.sigma_th = 0
 system.symbol_time = 1/system.symbol_rate
 system.Ts = system.symbol_time/system.N_sim
 system.Bw = 1/system.Ts
-system.pulse_shape = torch.exp(-(torch.arange(-100,101))**2/200)
+system.pulse_shape = np.exp(-(np.arange(-100,101))**2/60)
 system.responsivity = 1
 system.on_off = 1
 
 
-signal = system.applay_channel(torch.tensor([1]*4))
+signal = system.pulse_shaping([1])
 plt.figure(0)
+plt.plot(signal)
+
+signal = system.applay_channel(signal)
 
 signal = system.square_law_detection(signal)
+plt.plot(signal/np.max(signal))
 
-plt.plot(signal)
 
 plt.show()
