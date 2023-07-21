1771f09c8d5fda23a85d76bcc0166d03d7831749
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-21 12:33:04 +0200

    system working good in frequency domain

--- code/DD_system.py
+++ code/DD_system.py
@@ -4,46 +4,57 @@ class DD_system():
 
     N_sim = None
     N_os = None
-    pulse_shape = None
-    beta_2 = None
-    fiber_alpha = None 
-    fiber_length = None
-    Ts = None
-    symbol_time = None
-    symbol_rate = None
-    Bw = None
+    g_tx_td = None
+    G_tx_fd = None
+    channel_td = None
+    channel_fd = None
+    g_rx_td = None
+    G_rx_fd = None
+    len_fft = None
+
     responsivity = None
     sigma_sh = None
     sigma_th = None
-    rng = None
-    on_off = None
+    on_off_noise = None
 
 
     def __init__(self):    
         pass
 
 
-    def pulse_shaping(self,symbols):
+
+
+
+
+
+
+
+
+    
+
+    def simulate_system_fd(self, symbols):
         symbols_up_samp = np.zeros(int(len(symbols)*self.N_sim), dtype=complex)
         symbols_up_samp[self.N_sim-1::self.N_sim] = symbols
-        return np.convolve(symbols_up_samp,self.pulse_shape)
-    
-    def applay_channel(self,signal):
-        n_fft = int(2**np.ceil(np.log2(len(signal))))
-        f = self.Bw * np.fft.fftfreq(n_fft)
-        alpha_lin = self.fiber_alpha / (10 * np.log10(np.exp(1)))
-        channel_response_fft = np.exp(-alpha_lin*self.fiber_length + 1j*self.beta_2*self.fiber_length*(2*np.pi*f)**2/2)
-        signal_fft = np.fft.fft(signal, n=n_fft)
-        out = np.fft.ifft(channel_response_fft*signal_fft, n=n_fft)
-        return out[:len(signal)]
-    
+
+        signal_fd = np.fft.fft(symbols_up_samp, n=self.len_fft)
+        signal_fd *= self.G_tx_fd
+        signal_fd *= self.channel_fd
+
+        signal_td = np.fft.ifft(signal_fd, n=self.len_fft)
+        signal_td = self.square_law_detection(signal_td)
+
+        signal_fd = np.fft.fft(signal_td, n=self.len_fft)
+        signal_fd *= self.G_rx_fd
+
+        signal_td = np.fft.ifft(signal_fd, n=self.len_fft)
+        return signal_td[self.N_sim-1::int(self.N_sim/self.N_os)]
+        
     def square_law_detection(self,signal):
         abs_signal = np.abs(signal)
         square_law_signal = self.responsivity*abs_signal**2
         shot_noise = abs_signal * np.random.normal(0, self.sigma_sh, size=(len(signal)))
         thermal_noise = np.random.normal(0, self.sigma_th, size=(len(signal)))
         return square_law_signal + (shot_noise + thermal_noise)*self.on_off
-    
     
 
     
--- code/main.py
+++ code/main.py
@@ -1,33 +1,45 @@
 import numpy as np
-import DD_system
 import matplotlib.pyplot as plt
 
-
-system = DD_system.DD_system()
-system.N_sim = 4
-system.beta_2 = -2.168e-23
-system.fiber_alpha = 0.2
-system.fiber_length = 30
-system.symbol_rate = 35e9
-system.sigma_sh = 0
-system.sigma_th = 0
-system.symbol_time = 1/system.symbol_rate
-system.Ts = system.symbol_time/system.N_sim
-system.Bw = 1/system.Ts
-system.pulse_shape = np.sinc(np.arange(-10,10,0.25))
-print(np.arange(-2.5,2.75,0.25))
-system.responsivity = 1
-system.on_off = 1
+import DD_system
+import calc_filters
 
 
-signal = system.pulse_shaping([1]*1)
+DD_sys = DD_system.DD_system()
+DD_sys.N_sim = 4
+DD_sys.N_os = 2
+DD_sys.sigma_sh = 0
+DD_sys.sigma_th = 0
+DD_sys.responsivity = 1
+DD_sys.on_off_noise = 1
+
+
+symbol_rate = 35e9
+symbol_time = 1/symbol_rate
+Ts = symbol_time/DD_sys.N_sim
+fs = 1/Ts
+
+N_symbols = 100
+min_zero_padd = 100
+
+DD_sys.len_fft = int(2**np.ceil(np.log2(N_symbols*DD_sys.N_sim+min_zero_padd)))
+DD_sys.G_tx_fd = calc_filters.fd_rc_fd(alpha=0.2, fs=fs, sym_time=symbol_time, len=DD_sys.len_fft)
+DD_sys.channel_fd = calc_filters.CD_fiber_fd(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=30, len=DD_sys.len_fft, fs=fs)
+DD_sys.G_rx_fd = calc_filters.fd_rc_fd(alpha=0, fs=fs, sym_time=symbol_time/2, len=DD_sys.len_fft)
+
+symbols = np.zeros(N_symbols)
+symbols[50] = 1
+xd = DD_sys.simulate_system_fd(symbols)
+
+
+# signal = system.pulse_shaping([1]*1)
 plt.figure(0)
-plt.stem(signal)
+plt.stem(xd)
 
-signal = system.applay_channel(signal)
+# signal = system.applay_channel(signal)
 
-signal = system.square_law_detection(signal)
-plt.stem(signal/np.max(signal),markerfmt='+')
+# signal = system.square_law_detection(signal)
+# plt.stem(signal/np.max(signal),markerfmt='+')
 
 
 plt.show()
