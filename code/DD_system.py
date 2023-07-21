26d509b850e70d4a79794d5fbf19b7d225ab86a4
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-21 17:35:56 +0200

    system working in time and frequency domain and some test on ambiguity

--- code/DD_system.py
+++ code/DD_system.py
@@ -11,6 +11,7 @@ class DD_system():
     g_rx_td = None
     G_rx_fd = None
     len_fft = None
+    Ts = None
 
     responsivity = None
     sigma_sh = None
@@ -47,14 +48,31 @@ class DD_system():
         signal_fd *= self.G_rx_fd
 
         signal_td = np.fft.ifft(signal_fd, n=self.len_fft)
-        return signal_td[self.N_sim-1::int(self.N_sim/self.N_os)]
+        delay = int(self.N_sim-1)
+        stop = int(delay+len(symbols)*self.N_os)
+        return signal_td[delay:stop:int(self.N_sim/self.N_os)]
+
+    def simulate_system_td(self, symbols):
+        symbols_up_samp = np.zeros(int(len(symbols)*self.N_sim), dtype=complex)
+        symbols_up_samp[self.N_sim-1::self.N_sim] = symbols
+
+        signal =  np.convolve(symbols_up_samp, self.g_tx_td)*self.Ts
+        signal =  np.convolve(signal, self.channel_td)
         
+        signal = self.square_law_detection(signal)
+
+        signal =  np.convolve(signal, self.g_rx_td)*self.Ts
+        delay = int(self.N_sim+(len(self.g_tx_td)+len(self.channel_td)+len(self.g_rx_td)-3)/2-1)
+        stop = int(delay+len(symbols)*self.N_os)
+        return signal[delay:stop:int(self.N_sim/self.N_os)]
+
+
     def square_law_detection(self,signal):
         abs_signal = np.abs(signal)
         square_law_signal = self.responsivity*abs_signal**2
         shot_noise = abs_signal * np.random.normal(0, self.sigma_sh, size=(len(signal)))
         thermal_noise = np.random.normal(0, self.sigma_th, size=(len(signal)))
-        return square_law_signal + (shot_noise + thermal_noise)*self.on_off
+        return square_law_signal + (shot_noise + thermal_noise)*self.on_off_noise
     
 
     
--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ import calc_filters
 
 
 DD_sys = DD_system.DD_system()
-DD_sys.N_sim = 4
+DD_sys.N_sim = 2
 DD_sys.N_os = 2
 DD_sys.sigma_sh = 0
 DD_sys.sigma_th = 0
@@ -16,25 +16,44 @@ DD_sys.on_off_noise = 1
 
 symbol_rate = 35e9
 symbol_time = 1/symbol_rate
-Ts = symbol_time/DD_sys.N_sim
-fs = 1/Ts
+DD_sys.Ts = symbol_time/DD_sys.N_sim
+fs = 1/DD_sys.Ts
 
-N_symbols = 100
+N_symbols = 24
 min_zero_padd = 100
 
 DD_sys.len_fft = int(2**np.ceil(np.log2(N_symbols*DD_sys.N_sim+min_zero_padd)))
 DD_sys.G_tx_fd = calc_filters.fd_rc_fd(alpha=0.2, fs=fs, sym_time=symbol_time, len=DD_sys.len_fft)
-DD_sys.channel_fd = calc_filters.CD_fiber_fd(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=30, len=DD_sys.len_fft, fs=fs)
+DD_sys.channel_fd = calc_filters.CD_fiber_fd(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=0, len=DD_sys.len_fft, fs=fs)
 DD_sys.G_rx_fd = calc_filters.fd_rc_fd(alpha=0, fs=fs, sym_time=symbol_time/2, len=DD_sys.len_fft)
 
-symbols = np.zeros(N_symbols)
-symbols[50] = 1
-xd = DD_sys.simulate_system_fd(symbols)
+DD_sys.g_tx_td = calc_filters.fd_rc_td(alpha=0, fs=fs, sym_time=symbol_time, len=5)
+DD_sys.channel_td = calc_filters.CD_fiber_td(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=0, len=101, fs=fs)
+DD_sys.g_rx_td = calc_filters.fd_rc_td(alpha=0, fs=fs, sym_time=symbol_time/2, len=1)
 
+sign_1 = np.random.randint(2, size=N_symbols)*2-1
+sign_2 = np.random.randint(2, size=N_symbols)*2-1
 
+d_angles = np.random.randint(4, size=N_symbols)*2*np.pi/4
+symbols_1 = np.exp(1j*np.cumsum(d_angles*sign_1))
+symbols_2 = np.exp(1j*np.cumsum(d_angles*sign_2))
+
+d_symbols_1 = symbols_1[1:]/symbols_1[:-1]
+d_symbols_2 = symbols_2[1:]/symbols_2[:-1]
+
+x_1 = DD_sys.simulate_system_td(d_symbols_1)
+x_2 = DD_sys.simulate_system_td(d_symbols_2)
 # signal = system.pulse_shaping([1]*1)
 plt.figure(0)
-plt.stem(xd)
+plt.stem(x_1, markerfmt='o')
+plt.stem(x_2, markerfmt='*')
+
+plt.figure(1)
+plt.stem(np.angle(d_symbols_1)/np.pi, markerfmt='o')
+plt.stem(np.angle(d_symbols_2)/np.pi, markerfmt='*')
+# plt.figure(2)
+# plt.stem(DD_sys.g_tx_td, markerfmt='o')
+# plt.plot(np.imag(np.fft.fftshift(DD_sys.G_tx_fd*calc_filters.CD_fiber_fd(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=30, len=DD_sys.len_fft, fs=fs))))
 
 # signal = system.applay_channel(signal)
 
