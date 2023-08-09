ed618836af622233e2a74c7aecd04711e88683e7
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-08-09 14:18:33 +0200

    some tests

--- code/main.py
+++ code/main.py
@@ -27,13 +27,13 @@ fiber_len_km = 0
 
 ################# Simulation definition ####################
 sim_in_time_domain = True
-sim_in_freq_domain = True
+sim_in_freq_domain = False
 N_symbols = 101
 
 if sim_in_time_domain:
-    pulse_shape_len = 501
+    pulse_shape_len = 31
     channel_filt_len = 101
-    rx_filt_len = 501
+    rx_filt_len = 1
 
     DD_sys.g_tx_td = calc_filters.fd_rc_td(rc_alpha, pulse_shape_len, fs, symbol_time)
     DD_sys.channel_td = calc_filters.CD_fiber_td(alpha_dB_km, beta_2_s2_km, fiber_len_km, channel_filt_len, fs)
@@ -48,29 +48,37 @@ if sim_in_freq_domain:
 
 
 
+# test 1
+# sign_1 = np.random.randint(2, size=N_symbols)*2-1
+# sign_2 = np.random.randint(2, size=N_symbols)*2-1
 
-sign_1 = np.random.randint(2, size=N_symbols)*2-1
-sign_2 = np.random.randint(2, size=N_symbols)*2-1
+# d_angles = np.random.random(size=N_symbols)*2*np.pi
+# symbols_1 = np.exp(1j*np.cumsum(d_angles*sign_1))
+# symbols_2 = np.exp(1j*np.cumsum(d_angles*sign_2))
 
-d_angles = np.random.randint(4, size=N_symbols)*2*np.pi/4
-symbols_1 = np.exp(1j*np.cumsum(d_angles*sign_1))
-symbols_2 = np.exp(1j*np.cumsum(d_angles*sign_2))
+# d_symbols_1 = symbols_1[1:]/symbols_1[:-1]
+# d_symbols_2 = symbols_2[1:]/symbols_2[:-1]
 
-d_symbols_1 = symbols_1[1:]/symbols_1[:-1]
-d_symbols_2 = symbols_2[1:]/symbols_2[:-1]
+# test 2
+constellation = np.array([1, -1, 1j, -1j])
+
+index = np.random.randint(4, size=N_symbols)
+symbols_1 = constellation[index]
+symbols_2 = constellation[np.clip(index,0,2)]
+
 
 xd = np.random.randint(2, size=N_symbols)*2-1
-x_fd = DD_sys.simulate_system_fd(xd)
-x_td = DD_sys.simulate_system_td(xd)
+x_fd = DD_sys.simulate_system_td(symbols_1)
+x_td = DD_sys.simulate_system_td(symbols_2)
 
 plt.figure(0)
 plt.stem(x_fd, markerfmt='o', label='fd')
 plt.stem(x_td, markerfmt='*', label='td')
 plt.legend()
 
-# plt.figure(1)
-# plt.stem(np.angle(d_symbols_1)/np.pi, markerfmt='o')
-# plt.stem(np.angle(d_symbols_2)/np.pi, markerfmt='*')
+plt.figure(1)
+plt.stem(np.angle(symbols_1)/np.pi, markerfmt='o')
+plt.stem(np.angle(symbols_2)/np.pi, markerfmt='*')
 # plt.figure(2)
 # plt.stem(DD_sys.g_tx_td, markerfmt='o')
 # plt.plot(np.imag(np.fft.fftshift(DD_sys.G_tx_fd*calc_filters.CD_fiber_fd(alpha_dB_km=0.2, beta_2_s2_km=-2.168e-23, fiber_len_km=30, len=DD_sys.len_fft, fs=fs))))
