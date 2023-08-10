a600e46f150ce3a22936e298c6b2f4891a866e1b
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-08-10 16:25:05 +0200

    small issue

--- code/main.py
+++ code/main.py
@@ -32,13 +32,16 @@ sim_in_freq_domain = True
 N_symbols = 5
 
 if sim_in_time_domain:
-    pulse_shape_len = 11
+    pulse_shape_len = 3
     channel_filt_len = 11
-    rx_filt_len = 11
+    rx_filt_len = 1
 
-    DD_sys.g_tx_td = torch.tensor([[calc_filters.fd_rc_td(rc_alpha, pulse_shape_len, fs, symbol_time)]], dtype=torch.complex128)
-    DD_sys.channel_td = torch.tensor([[calc_filters.CD_fiber_td(alpha_dB_km, beta_2_s2_km, fiber_len_km, channel_filt_len, fs)]], dtype=torch.complex128)
-    DD_sys.g_rx_td = torch.tensor([[calc_filters.fd_rc_td(0, rx_filt_len, fs, symbol_time/2)]], dtype=torch.float64)
+    g_tx_td = torch.tensor(calc_filters.fd_rc_td(rc_alpha, pulse_shape_len, fs, symbol_time), dtype=torch.complex128)
+    DD_sys.g_tx_td = g_tx_td[None, None,:]
+    channel_td = torch.tensor(calc_filters.CD_fiber_td(alpha_dB_km, beta_2_s2_km, fiber_len_km, channel_filt_len, fs), dtype=torch.complex128)
+    DD_sys.channel_td = channel_td[None, None,:]
+    g_rx_td = torch.tensor(calc_filters.fd_rc_td(0, rx_filt_len, fs, symbol_time/2), dtype=torch.float64)
+    DD_sys.g_rx_td = g_rx_td[None, None,:]
 if sim_in_freq_domain:
     min_zero_padd = 300
 
@@ -48,21 +51,15 @@ if sim_in_freq_domain:
     DD_sys.G_rx_fd = calc_filters.fd_rc_fd(0, DD_sys.len_fft, fs, symbol_time/2) 
 
 
-symbols = torch.zeros(1,30, dtype=torch.complex128)
-symbols[0,15] = 1
-sym = torch.tensor([[3, 0, 1, 0, 0, 1, 3, 3, 2, 1, 3, 0, 3, 1, 0, 1, 1, 3, 0, 0, 0, 0, 2, 2, 0, 1, 0, 2, 3, 2]], dtype=torch.complex128)
-x = DD_sys.simulate_system_fd(sym)
-print(x.size())
 # test 1
-# sign_1 = np.random.randint(2, size=N_symbols)*2-1
-# sign_2 = np.random.randint(2, size=N_symbols)*2-1
+sign_1 = torch.randint(2, size=(1,N_symbols))*2-1
+sign_2 = torch.randint(2, size=(1,N_symbols))*2-1
 
-# d_angles = np.random.random(size=N_symbols)*2*np.pi
-# symbols_1 = np.exp(1j*np.cumsum(d_angles*sign_1))
-# symbols_2 = np.exp(1j*np.cumsum(d_angles*sign_2))
-
-# d_symbols_1 = symbols_1[1:]/symbols_1[:-1]
-# d_symbols_2 = symbols_2[1:]/symbols_2[:-1]
+d_angles = torch.rand(size=(N_symbols,))*2*np.pi
+symbols_1 = torch.exp(1j*torch.cumsum(d_angles*sign_1, dim=0)).type(torch.complex128)
+symbols_2 = torch.exp(1j*torch.cumsum(d_angles*sign_2, dim=0)).type(torch.complex128)
+d_symbols_1 = symbols_1[1:]/symbols_1[:-1]
+d_symbols_2 = symbols_2[1:]/symbols_2[:-1]
 
 # test 2
 # constellation = torch.tensor([1, -1, np.sqrt(2)/2+np.sqrt(2)/2*1j, -np.sqrt(2)/2-np.sqrt(2)/2*1j])
@@ -73,15 +70,14 @@ print(x.size())
 # symbols_3 = -constellation[index]
 # symbols_4 = -constellation[(index+2)%4]
 
-# xd = np.random.randint(2, size=N_symbols)*2-1
-# x_1 = DD_sys.simulate_system_td(symbols_1)
-# x_2 = DD_sys.simulate_system_td(symbols_2)
+x_1 = DD_sys.simulate_system_td(symbols_1)
+x_2 = DD_sys.simulate_system_td(symbols_2)
 # x_3 = DD_sys.simulate_system_td(symbols_3)
 # x_4 = DD_sys.simulate_system_td(symbols_4)
 
 plt.figure(0)
-plt.stem(x[0,:], markerfmt='o', label='fd')
-# plt.stem(x_2, markerfmt='*', label='td')
+plt.stem(torch.real(x_1[0,:]), markerfmt='o', label='fd')
+plt.stem(torch.real(x_2[0,:]), markerfmt='*', label='td')
 # plt.stem(x_3, markerfmt='o', label='fd')
 # plt.stem(x_4, markerfmt='*', label='td')
 # plt.legend()
