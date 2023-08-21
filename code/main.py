d2cdb37872de308be555c4a485fe03ea41bd0d77
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-08-21 15:23:56 +0200

    system working good for PAM, (no differential decoding)

--- code/main.py
+++ code/main.py
@@ -6,6 +6,7 @@ import DD_system
 import calc_filters
 import constellation
 import bcjr_upsamp
+import channel_metrics as ch_met
 
 #################### System definition ##################
 DD_sys = DD_system.DD_system()
@@ -21,7 +22,7 @@ rc_alpha = 0
 DD_sys.sigma_sh = 0
 DD_sys.sigma_th = 1
 DD_sys.responsivity = 1
-DD_sys.on_off_noise = 0
+DD_sys.on_off_noise = 1
 
 ################# Channel definition ###################
 alpha_dB_km = 0.2
@@ -29,7 +30,7 @@ beta_2_s2_km = -2.168e-23
 fiber_len_km = 0
 
 ################# filters creation definition ###################
-pulse_shape_len = 15
+pulse_shape_len = 5
 channel_filt_len = 1
 rx_filt_len = 1
 
@@ -41,7 +42,7 @@ g_rx_td = torch.tensor(calc_filters.fd_rc_td(0, rx_filt_len, fs, symbol_time/2),
 DD_sys.g_rx_td = g_rx_td[None, None,:]
 
 ################# Simulation definition ####################
-N_symbols = 20
+N_symbols = 20_000
 
 ###################### Constellation #########################
 
@@ -49,29 +50,32 @@ N_symbols = 20
 SNR_dB = 4
 
 
-mapping = torch.tensor([1,-1], dtype=torch.cfloat)
+mapping = torch.tensor([1,2,3,4], dtype=torch.cfloat)
 SNR_lin = 10**(SNR_dB/10)
 mapping *= torch.sqrt(SNR_lin/torch.mean(torch.abs(mapping)**2))
 const = constellation.constellation(mapping,'cpu')
 
 bits = torch.randint(2,(N_symbols*const.m,))
 
-symbols = torch.concat((mapping[0:1],const.map(bits)))
+symbols = const.map(bits)
 
 y_1 = DD_sys.simulate_system_td(symbols[None,:])
-y_2 = DD_sys.simulate_system_td(-symbols[None,:])
 
-decoder = bcjr_upsamp.bcjr_upsamp(DD_sys.g_tx_td[0,0,:], 0, N_symbols+1, const, DD_sys.N_os)
+decoder = bcjr_upsamp.bcjr_upsamp(DD_sys.g_tx_td[0,0,:], 0, N_symbols, const, DD_sys.N_os)
 beliefs = decoder.compute_true_apps(y_1, log_out=False, P_s0=None)#(torch.eye(const.M)[0:1,:]-1)*1e10)
 
-print(DD_sys.g_tx_td)
-print(beliefs)
-print(torch.sum(bits != torch.argmax(beliefs[0,1:], dim=1))/N_symbols)
+symbols_hat_idx = torch.argmax(beliefs[0,0:], dim=1)
+bits_hat = const.demap(symbols_hat_idx)
+symbols_hat = const.map(bits_hat.int())
+
+
+print(f"BER = {ch_met.get_ER(bits, bits_hat): .3f}")
+print(f"SER = {ch_met.get_ER(symbols, symbols_hat): .3f}")
 
 plt.figure()
-plt.stem(y_1[0,:], markerfmt="o")
-plt.stem(y_2[0,:], markerfmt='*')
-# plt.scatter(range(len(beliefs[0,:,0])),beliefs[0,:,0])
+# plt.stem(y_1[0,:], markerfmt="o")
+# plt.stem(y_2[0,:], markerfmt='*')
+plt.scatter(range(len(beliefs[0,:,0])),beliefs[0,:,0])
 plt.grid()
 
 plt.show()
