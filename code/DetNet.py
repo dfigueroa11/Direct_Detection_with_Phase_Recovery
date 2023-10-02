10a8ef0904eba4fb50bbcd3f34e5260f242af5e6
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-10-02 16:01:29 +0200

    functions to convert between one hot and symbol notation tested and working properly

--- code/DetNet.py
+++ code/DetNet.py
@@ -19,9 +19,9 @@ print("We are using the following device for learning:",device)
 
 
 # System config
-sym_mem = 8
+sym_mem = 3
 ch_mem = 2*sym_mem+1
-block_len = 30
+block_len = 20
 snr_dB = 10
 snr_dB_var = 2
 
@@ -32,9 +32,11 @@ mapping *= torch.sqrt(1/torch.mean(torch.abs(mapping)**2))
 const = constellation.constellation(mapping, device ,diff_mapping)
 
 ################### training hyperparameters #######################
-batch_size = 1
+batch_size = 2
 
 
 
 
 y_e, y_o, Psi_e, Psi_o, tx_syms = aux_func.data_generation(block_len, sym_mem, batch_size, snr_dB, snr_dB_var, const, device)
+tx_syms_oh = aux_func.sym_2_oh(const.mapping_re, const.mapping_im, tx_syms, device)
+tx_syms_hat = aux_func.oh_2_sym(const.mapping_re, const.mapping_im, tx_syms_oh, block_len+sym_mem, device)
