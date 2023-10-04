25f8f6c8766bc138b23ce429a6a54cfc85def6fa
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-10-04 15:25:47 +0200

    DetNet architecture working good

--- code/DetNet_architecture.py
+++ code/DetNet_architecture.py
@@ -5,9 +5,10 @@ import DetNet_aux_functions as aux_func
 
 class DetNet(nn.Module):
 
-    def __init__(self, layers, sym_len, one_hot_len, v_len, z_len, device):
+    def __init__(self, layers, block_len, sym_mem , one_hot_len, v_len, z_len, device):
         super(DetNet, self).__init__()
         # define the parameters for the linear transformation: (W1,b1), (W2,b2) and (W3,b3)
+        sym_len = block_len + sym_mem
         self.linear_trafo_1_l = nn.ModuleList()
         self.linear_trafo_1_l.extend([nn.Linear(2*(sym_len + v_len), 2*z_len) for i in range(layers)]) #2* because of real and imag part
         for i in range(layers):
@@ -46,39 +47,42 @@ class DetNet(nn.Module):
         self.layers = layers
         self.v_len = v_len
         self.sym_len = sym_len
+        self.block_len = block_len
+        self.sym_mem = sym_mem
         self.one_hot_len = one_hot_len
         self.device = device
             
     def forward(self, y_e, y_o, Psi_e, Psi_o, mapp_re, mapp_im):
         batch_size = y_e.size(0)
-        v = torch.zeros(batch_size, self.v_len, device=self.device)
-        x = torch.zeros(1, batch_size, self.sym_len, device=self.device)
+        v = torch.zeros(batch_size, 2*self.v_len, device=self.device)
+        x = torch.zeros(1, batch_size, 2*self.sym_len, device=self.device)
         x_oh = torch.zeros(1, batch_size, self.sym_len*self.one_hot_len, device=self.device)
-        
         # Send Data through the staced DetNet
         for l in range(self.layers):
-            # calculate the gradient: q_k
-            Psi_e_x = torch.matmul(Psi_e,x[-1])
-            diag_Psi_e_x = torch.cat(torch.diag_embed(Psi_e_x[:self.sym_len]),torch.diag_embed(Psi_e_x[self.sym_len:]))
-            A_e = torch.matmul(torch.transpose(Psi_e, dim0=1, dim1=2),diag_Psi_e_x)
+            # calculate the gradient: q
+            Psi_e_x = torch.bmm(Psi_e,x[-1].unsqueeze(-1)).squeeze(-1)
+            diag_Psi_e_x = torch.cat((torch.diag_embed(Psi_e_x[:,:self.block_len]),torch.diag_embed(Psi_e_x[:,self.block_len:])),1)
+            A_e = torch.bmm(torch.transpose(Psi_e, dim0=1, dim1=2),diag_Psi_e_x)
+            
+            Psi_o_x = torch.bmm(Psi_o,x[-1].unsqueeze(-1)).squeeze(-1)
+            diag_Psi_o_x = torch.cat((torch.diag_embed(Psi_o_x[:,:self.block_len]),torch.diag_embed(Psi_o_x[:,self.block_len:])),1)
+            A_o = torch.bmm(torch.transpose(Psi_o, dim0=1, dim1=2),diag_Psi_o_x)
 
-            Psi_o_x = torch.matmul(Psi_o,x[-1])
-            diag_Psi_o_x = torch.cat(torch.diag_embed(Psi_o_x[:self.sym_len]),torch.diag_embed(Psi_o_x[self.sym_len:]))
-            A_o = torch.matmul(torch.transpose(Psi_o, dim0=1, dim1=2),diag_Psi_o_x)
+            Psi_e_x_sql = torch.sum(torch.square(Psi_e_x).reshape(batch_size,2,-1),dim=1).unsqueeze(-1)
+            Psi_o_x_sql = torch.sum(torch.square(Psi_o_x).reshape(batch_size,2,-1),dim=1).unsqueeze(-1)
             
-            q = x[-1] - self.delta1_l[l]*torch.matmul(A_e,y_e) + self.delta2_l[l]*torch.matmul(A_e,torch.square(Psi_e_x)) \
-                - self.delta2_l[l]*torch.matmul(A_o,y_o) + self.delta4_l[l]*torch.matmul(A_o,torch.square(Psi_o_x))
+            q = x[-1] - self.delta1_l[l]*torch.bmm(A_e,y_e.unsqueeze(-1)).squeeze(-1) + self.delta2_l[l]*torch.bmm(A_e,Psi_e_x_sql).squeeze(-1) \
+                - self.delta2_l[l]*torch.bmm(A_o,y_o.unsqueeze(-1)).squeeze(-1) + self.delta4_l[l]*torch.bmm(A_o,Psi_o_x_sql).squeeze(-1)
             
-            # Apply linear transformation and rectified linear unit (ReLU)
+            # Apply linear transformation and ReLU
             z = self.relu(self.linear_trafo_1_l[l](torch.cat((q, v), 1)))
             # Apply linear transformation
-            x_oh = torch.cat((x_oh, x_oh[-1] + self.linear_trafo_2_l[l](z)))
-            # proyect and ap
-            x = torch.cat((x, aux_func.oh_2_sym(mapp_re , mapp_im, x_oh[-1], self.sym_len, self.device)), 0)
-            
+            x_oh = torch.cat((x_oh, x_oh[-1:] + self.linear_trafo_2_l[l](z)))
+            # proyect and append result
+            x = torch.cat((x, aux_func.oh_2_sym(mapp_re , mapp_im, x_oh[-1], self.sym_len, self.device).unsqueeze(0)), 0)
             # Generate new v iterate with a final linear trafo.
             v = v + self.linear_trafo_3_l[l](z)
-            del Psi_e_x, A_e, diag_Psi_e_x, Psi_o_x, A_o, q, z 
+            del Psi_e_x, diag_Psi_e_x, A_e, Psi_o_x, diag_Psi_o_x, A_o, Psi_e_x_sql, Psi_o_x_sql, q, z 
             torch.cuda.empty_cache()
         del v
         torch.cuda.empty_cache()
--- code/DetNet_training.py
+++ code/DetNet_training.py
@@ -12,6 +12,7 @@ import bcjr_upsamp
 import channel_metrics as ch_met
 import constellation_maker as const_mk
 import DetNet_aux_functions as aux_func
+import DetNet_architecture
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -21,7 +22,8 @@ print("We are using the following device for learning:",device)
 # System config
 sym_mem = 3
 ch_mem = 2*sym_mem+1
-block_len = 20
+block_len = 7
+sym_len = block_len+sym_mem
 snr_dB = 10
 snr_dB_var = 2
 
@@ -32,11 +34,24 @@ mapping *= torch.sqrt(1/torch.mean(torch.abs(mapping)**2))
 const = constellation.constellation(mapping, device ,diff_mapping)
 
 ################### training hyperparameters #######################
-batch_size = 2
+training_steps = 1
+batch_size_train = 2
+
+############################ DetNet declaration #####################
+layers = 3*sym_len
+v_len = 2*sym_len
+z_len = 4*sym_len
+one_hot_len = len(const.mapping_re) + len(const.mapping_im)
+
+
+model = DetNet_architecture.DetNet(layers, block_len, sym_mem, one_hot_len, v_len, z_len, device)
+model.to(device)
 
 
 
-
-y_e, y_o, Psi_e, Psi_o, tx_syms = aux_func.data_generation(block_len, sym_mem, batch_size, snr_dB, snr_dB_var, const, device)
+y_e, y_o, Psi_e, Psi_o, tx_syms = aux_func.data_generation(block_len, sym_mem, batch_size_train, snr_dB, snr_dB_var, const, device)
 tx_syms_oh = aux_func.sym_2_oh(const.mapping_re, const.mapping_im, tx_syms, device)
-tx_syms_hat = aux_func.oh_2_sym(const.mapping_re, const.mapping_im, tx_syms_oh, block_len+sym_mem, device)
+tx_syms_hat = aux_func.oh_2_sym(const.mapping_re, const.mapping_im, tx_syms_oh, sym_len, device)
+
+
+x, x_oh = model(y_e, y_o, Psi_e, Psi_o, const.mapping_re, const.mapping_im)
