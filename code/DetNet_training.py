19abda78f48d2e727fa3b5f42c352293862f64c3
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-11-05 03:16:52 +0100

    train for sym_mem = 5, and SNR 18 dB

--- code/DetNet_training.py
+++ code/DetNet_training.py
@@ -72,7 +72,7 @@ if resume_training:
 # hyperparameters
 batches_per_epoch = 300
 batch_size_per_epoch = [100, 400, 1_000, 2_000, 5_000, 10_000]
-snr_dB_steps = [*range(17,18)]
+snr_dB_steps = [*range(18,19)]
 checkpoint_per_epoch = 5
 cnt = checkpoint['cnt'] if resume_training else 0
 
