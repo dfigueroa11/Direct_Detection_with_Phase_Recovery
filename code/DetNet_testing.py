c45b7c676fc8abb86d4711b2bcd2246594267e33
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-11-24 15:21:47 +0100

    test

--- code/DetNet_testing.py
+++ code/DetNet_testing.py
@@ -78,13 +78,15 @@ for sym_mem_idx, sym_mem_file in enumerate(sym_mem_file_list):
             if i%(N_symbols//10) == 0:
                 print(f'\t\t symbol number {i}')
             mag, phase = magphase_DetNet(y_e[:,i:i+block_len], y_o[:,i:i+block_len], Psi_e, Psi_o, state_mag, state_phase, layers, return_all=False)
+            mag = mag[:,0]
+            phase = phase[:,0]
             # rx_mag[:,i] = mag[:,0]
             # rx_phase[:,i] = phase[:,0]
             # #update state
             state_mag = torch.roll(state_mag,-1,-1)
-            state_mag[0,-1] = mag[:,0]
+            # state_mag[0,-1] = mag[:,0]
             state_phase = torch.roll(state_phase,-1,-1)
-            state_phase[0,-1] = phase[:,0]
+            # state_phase[0,-1] = phase[:,0]
             del mag, phase
             torch.cuda.empty_cache()
             
