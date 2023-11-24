190e2cd2512046275f6fce76690a093f62307566
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-11-24 13:52:47 +0100

    test

--- code/DetNet_testing.py
+++ code/DetNet_testing.py
@@ -71,8 +71,8 @@ for sym_mem_idx, sym_mem_file in enumerate(sym_mem_file_list):
                                                                                             snr_dB, 0, const, device, outPsi_mat=False)
         tx_mag = tx_mag[:,:-block_len]
         tx_phase = tx_phase[:,:-block_len]
-        rx_mag = torch.ones(tx_mag)
-        rx_phase = torch.ones(tx_phase)
+        rx_mag = torch.ones_like(tx_mag)
+        rx_phase = torch.ones_like(tx_phase)
         s = time.time()
         for i in range(3):
             if i%(N_symbols//10) == 0:
