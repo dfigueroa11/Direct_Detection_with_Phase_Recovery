2bdeb82664ff59d989f3046ff786b1d77191aefe
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-11-24 16:30:40 +0100

    test

--- code/DetNet_testing.py
+++ code/DetNet_testing.py
@@ -71,19 +71,21 @@ for sym_mem_idx, sym_mem_file in enumerate(sym_mem_file_list):
                                                                                                 snr_dB, 0, const, device, outPsi_mat=False)
             tx_mag = tx_mag[:,:-block_len]
             tx_phase = tx_phase[:,:-block_len]
-            rx_mag = torch.ones_like(tx_mag, device=device)
-            rx_phase = torch.ones_like(tx_phase, device=device)
+            rx_mag = torch.empty_like(tx_mag)
+            rx_phase = torch.empty_like(tx_phase)
             s = time.time()
             for i in range(N_symbols):
                 if i%(N_symbols//10) == 0:
                     print(f'\t\t symbol number {i}')
-                mag, phase = magphase_DetNet(y_e[:,i:i+block_len], y_o[:,i:i+block_len], Psi_e, Psi_o,
-                                            rx_mag[:,i:i+sym_mem], rx_phase[:,i:i+sym_mem], layers, return_all=False)
-                rx_mag[:,i] = mag[:,0]
-                rx_phase[:,i] = phase[:,0]
-                del mag, phase
-                torch.cuda.empty_cache()
-                
+                mag, phase = magphase_DetNet(y_e[:,i:i+block_len], y_o[:,i:i+block_len], Psi_e, Psi_o, state_mag, state_phase, layers, return_all=False)
+                rx_mag[:,i] = mag [:,0]
+                rx_phase[:,i] = phase [:,0]
+                #update state
+                state_mag = torch.roll(state_mag,-1,-1)
+                state_mag[0,-1] = mag[:,0]
+                state_phase = torch.roll(state_phase,-1,-1)
+                state_phase[0,-1] = phase[:,0]
+
             rx_syms = rx_mag*torch.exp(1j*rx_phase)
             rx_syms_idx = const.nearest_neighbor(rx_syms)
             e = time.time()
