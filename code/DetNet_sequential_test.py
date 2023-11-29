f92d85310f542169d8aa1af95f6bea5048d7f3d4
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-11-29 17:24:00 +0100

    test of sequential testing

--- code/DetNet_sequential_test.py
+++ code/DetNet_sequential_test.py
@@ -20,9 +20,9 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("We are using the following device for learning:",device)
 
 
-N_symbols = 2_000
+N_symbols = 1000
 
-snr_dB_list = [*range(15,16)]
+snr_dB_list = [*range(20,21)]
 sym_mem_file_list = [1,]
 ############# Constellation and differential mapping ################
 angle = np.arccos(1/3)
@@ -69,25 +69,26 @@ for sym_mem_idx, sym_mem_file in enumerate(sym_mem_file_list):
 
             y_e, y_o, tx_mag, tx_phase, state_mag, state_phase = aux_func.data_generation(N_symbols+block_len, sym_mem, 1,
                                                                                                 snr_dB, 0, const, device, outPsi_mat=False)
-            tx_mag = tx_mag[:,:-block_len]
-            tx_phase = tx_phase[:,:-block_len]
-            rx_mag = torch.empty_like(tx_mag)
-            rx_phase = torch.empty_like(tx_phase)
+            tx_mag = tx_mag[0,:-block_len]
+            tx_phase = tx_phase[0,:-block_len]
+            rx_syms_idx = torch.empty_like(tx_mag, dtype=torch.long)
             s = time.time()
             for i in range(N_symbols):
                 if i%(N_symbols//10) == 0:
                     print(f'\t\t symbol number {i}')
                 mag, phase = magphase_DetNet(y_e[:,i:i+block_len], y_o[:,i:i+block_len], Psi_e, Psi_o, state_mag, state_phase, layers, return_all=False)
-                rx_mag[:,i] = mag [:,0]
-                rx_phase[:,i] = phase [:,0]
+                rx_sym = mag[0,0]*torch.exp(1j*phase[0,0])
+                rx_syms_idx[i] = const.nearest_neighbor(rx_sym)
+                rx_sym = const.mapping[rx_syms_idx[i]]  # info symbol
                 #update state
+                info_sym_phase_idx = torch.argmin(torch.abs(const.phase_list-torch.angle(rx_sym)))
+                prev_phase_idx = torch.argmin(torch.abs(const.phase_list-state_phase[0,-1]))
+                diff_phase = const.phase_list[const.diff_mapping[info_sym_phase_idx,prev_phase_idx]]
                 state_mag = torch.roll(state_mag,-1,-1)
-                state_mag[0,-1] = mag[:,0]
+                state_mag[0,-1] = torch.abs(rx_sym)
                 state_phase = torch.roll(state_phase,-1,-1)
-                state_phase[0,-1] = phase[:,0]
+                state_phase[0,-1] = diff_phase
 
-            rx_syms = rx_mag*torch.exp(1j*rx_phase)
-            rx_syms_idx = const.nearest_neighbor(rx_syms)
             e = time.time()
 
             tx_syms = tx_mag*torch.exp(1j*tx_phase)
